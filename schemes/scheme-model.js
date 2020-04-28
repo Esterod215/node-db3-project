a3761d349d76ae0276ea923ce5d9e3fee616d60a
Estevan <estevan.rodriguez10@yahoo.com>
2020-04-28 15:39:25 -0700

    added find and find by Id methods

---
+++ schemes/scheme-model.js
@@ -0,0 +1,14 @@
+const knex = require("knex");
+const config = require("../knexfile");
+const db = knex(config.development);
+module.exports = {
+  find,
+  findById
+};
+
+function find() {
+  return db("schemes");
+}
+function findById(id) {
+  return db("schemes").where({ id });
+}
