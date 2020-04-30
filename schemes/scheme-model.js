e6326b7906097c6ff1631b7a89c86d1399ffa5fb
Estevan <estevan.rodriguez10@yahoo.com>
2020-04-29 18:48:34 -0700

    finished get, getById, post and post steps router endpoints

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -3,12 +3,45 @@ const config = require("../knexfile");
 const db = knex(config.development);
 module.exports = {
   find,
-  findById
+  findById,
+  findSteps,
+  add,
+  addStep
 };
 
 function find() {
   return db("schemes");
 }
 function findById(id) {
-  return db("schemes").where({ id });
+  return db("schemes")
+    .where({ id })
+    .first();
+}
+function findSteps(id) {
+  return db("schemes")
+    .join("steps", "steps.scheme_id", "schemes.id")
+    .select("schemes.scheme_name", "steps.step_number", "steps.instructions")
+    .where({ scheme_id: id })
+    .orderBy("steps.step_number");
+}
+function add(schemeData) {
+  return db("schemes")
+    .insert(schemeData)
+    .then(ids => {
+      return findById(ids[0]);
+    });
+}
+function addStep(stepData, id) {
+  const newStepData = {
+    step_number: stepData.step_number,
+    instructions: stepData.instructions,
+    scheme_id: id
+  };
+  console.log(newStepData);
+  return db("steps")
+    .insert(newStepData)
+    .then(ids => {
+      console.log("id we get back", ids);
+      return findSteps(id);
+    });
 }
